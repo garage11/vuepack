d690345f2f4cdb5c7eba9d770a40589cde7a645d
Jeroen van Veen <jvanveen@protonmail.com>
2020-08-11 18:23:34 +0200

    feat: add component imports & semantic release

--- index.js
+++ index.js
@@ -11,60 +11,81 @@ export default class VuePack {
         this.options = options
     }
 
+
     capitalize(s) {
         return s.charAt(0).toUpperCase() + s.slice(1)
     }
 
 
-    toFunction(code) {
-        return transpile(`function r(){${code}}`)
-    }
-
-    /**
-     * Generates a template/component names based on the
-     * path of the filename. A path filter can be used
-     * to trim down on unwanted path elements.
-     * @param {String} filename - Path to a template
-     * @returns {String} - The generated template name
-     */
-    fileToTemplateName(filename) {
-        let parts = []
-        // Remove file extension.
-        // let tmpName = filename.replace(path.extname(filename), '')
-        for (let part of filename.replace(path.extname(filename), '').split('/')) {
-            if (part !== '.' && !this.options.pathfilter.includes(part)) {
-                part = part.split('-').map((p) => this.capitalize(p)).join('')
-                parts.push(part)
-            }
-        }
-
-        // Filter out double names.
-        parts = parts.filter((value, index, self) => self.indexOf(value) === index)
-        parts = parts.map((part) => this.capitalize(part))
-
-        return parts.join('')
-    }
-
     async compile(filenames) {
-        let data = 'const _ = {};'
+        let templates = 'const _ = {};'
         const readFiles = await Promise.all(filenames.map((filename) => fs.readFile(filename, 'utf8')))
+        let components = ''
+        const componentNames = []
         filenames.forEach((filename, i) => {
             const templateData = readFiles[i]
-            const templateName = this.fileToTemplateName(filename)
+            const componentName = this.fileToComponentName(filename, this.options.pathFilter)
+            const importPath = this.fileToImportPath(filename, this.options.importFilter)
+            componentNames.push(componentName)
+
+            components += `import ${componentName} from '${importPath}'\r\n`
 
             let compiled = compiler.compile(templateData, {
                 preserveWhitespace: false,
             })
             if (compiled.errors.length) throw compiled.errors.join(',')
-            let jsTemplate = `_.${templateName}={r:${this.toFunction(compiled.render)}`
+            let jsTemplate = `_.${componentName}={r:${this.toFunction(compiled.render)}`
             if (compiled.staticRenderFns.length) {
                 jsTemplate += `,s:[${compiled.staticRenderFns.map(this.toFunction).join(',')}]};`
             } else jsTemplate += '};'
 
-            data += jsTemplate
+            templates += jsTemplate
         })
 
-        data += 'export default _'
-        return data
+        components += `export default {${componentNames.join(', ')}}`
+
+        templates += 'export default _'
+        return {components, templates}
+    }
+
+
+    /**
+     * Generate a component name based on the path of the filename.
+     * A path filter is used to trim down unwanted path parts.
+     * @param {String} filename - Path to a template
+     * @returns {String} - The generated template name
+     */
+    fileToComponentName(filename) {
+        let parts = this.filter(filename, this.options.pathFilter, true)
+        // Filter out double names.
+        parts = parts.filter((value, index, self) => self.indexOf(value) === index)
+        parts = parts.map((part) => this.capitalize(part))
+        return parts.join('')
+    }
+
+
+    fileToImportPath(filename) {
+        let parts = this.filter(filename, this.options.importFilter)
+        return `/${path.join(parts.join('/'))}.js`
+    }
+
+
+    filter(filename, filter, toPascalCase = false) {
+        let parts = []
+        for (let part of filename.replace(path.extname(filename), '').split('/')) {
+            if (part !== '.' && !filter.includes(part)) {
+                if (toPascalCase) {
+                    part = part.split('-').map((p) => this.capitalize(p)).join('')
+                }
+                parts.push(part)
+            }
+        }
+
+        return parts
+    }
+
+
+    toFunction(code) {
+        return transpile(`function r(){${code}}`)
     }
 }
